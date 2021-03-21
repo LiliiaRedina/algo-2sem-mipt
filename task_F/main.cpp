05680b5d7efd53d06e4b4be4d47a8c8eb5cdd44e
Liliiaa <lilia200264@gmail.com>
2021-03-21 23:33:25 +0300

    Modified task_F

--- task_F/main.cpp
+++ task_F/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <vector>
 
-void fill_dp_prefix(const std::vector<int> &arr1, const std::vector<int> &arr2,
-                 std::vector<std::vector<int>> &dp_prefix, const int &size1, const int &size2) {
-    for (int i = 1; i < size1 + 1; i++) {
-        for (int j = 1; j < size2 + 1; j++) {
+void fill_dp_prefix(const std::vector<int>& arr1, const std::vector<int>& arr2,
+                 std::vector<std::vector<int>>& dp_prefix) {
+    for (int i = 1; i < arr1.size() + 1; i++) {
+        for (int j = 1; j < arr2.size() + 1; j++) {
             if (arr1[i - 1] == arr2[j - 1]) {
                 dp_prefix[i][j] = dp_prefix[i - 1][j - 1] + 1;
             } else {
@@ -35,7 +35,7 @@ int main() {
 
     std::vector<std::vector<int>> dp_prefix(size1 + 1, std::vector<int>(size2 + 1, 0));
 
-    fill_dp_prefix(arr1, arr2, dp_prefix, size1, size2);
+    fill_dp_prefix(arr1, arr2, dp_prefix);
 
     std::cout << dp_prefix[size1][size2];
 
